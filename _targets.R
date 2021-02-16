1640bc9fc5999efbf2a63df7b65551d6cfe7c71c
rushgeo <10775395+rushgeo@users.noreply.github.com>
2021-02-16 17:32:39 +0000

    generate birth data

--- _targets.R
+++ _targets.R
@@ -15,6 +15,8 @@ tar_option_set(
   )
 )
 list(
+  # INPUT DATA
+
   #tar_target(unused ,"data/Births_byWeek_LMP_NYS_2018.txt", format = "file"),
   #tar_target(unused ,"data/Yr_Month_GestAge_Plurality_NYS_2007to18.txt", format = "file"),
   tar_target(Births_WklyGestAge_07to18_file, "data/Births_NYS_Year_SingletonGestAge.txt", format = "file"),
@@ -29,5 +31,20 @@ list(
   tar_target(Births_GestWeek_notInduced_file ,"data/NotInduced2018NYSBirths_singleton.txt", format = "file"),
   tar_target(NYBirths_by_Month_single_notInduced_file ,"data/NotInduced2018NYSBirths_bymonth_singleton.txt", format = "file"),
 
-  tar_target(LaGuardiaTemp1, load_temp(LaGuardiaTemp_file))
+  # FUNCTION OUTPUTS
+  tar_target(LaGuardiaTemp1, load_temp(LaGuardiaTemp_file)),
+  tar_target(NYBirths_by_Day,
+              Clean_and_smooth_data(NYBirths_by_Month_plural_file,
+                                    NYBirths_by_Weekday_file)),
+  tar_target(Preterms_per_day_all,
+              Estimate_all_daily_preterms(NYBirths_by_Day,
+                                          NYBirths_by_Month_single_file,
+                                          Births_WklyGestAge_07to18_file,
+                                          Annual_Singleton_Births_file)),
+  tar_target(Preterms_per_day_notInduced,
+              Estimate_nonInduced_daily_preterms(NYBirths_by_Day,
+                                                 NYBirths_by_Month_single_file,
+                                                 Births_GestWeek_notInduced_file,
+                                                 Annual_Singleton_Births_file,
+                                                 NYBirths_by_Month_single_notInduced_file))
 )
